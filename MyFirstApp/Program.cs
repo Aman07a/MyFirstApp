ff2f2347deda55e24a401c04b1b1bc0f2621fea5
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-21 09:40:50 +0100

    HTTP Response Headers

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -5,17 +5,12 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
-    if (1 == 1)
-    {
-        context.Response.StatusCode = 200;
-    }
-    else
-    {
-        context.Response.StatusCode = 400;
-    }
+    context.Response.Headers["MyKey"] = "my value";
+    context.Response.Headers["Server"] = "My server";
+    context.Response.Headers["Content-Type"] = "text/html";
 
-    await context.Response.WriteAsync("Hello");
-    await context.Response.WriteAsync(" World");
+    await context.Response.WriteAsync("<h1>Hello</h1>");
+    await context.Response.WriteAsync("<h2>World</h2>");
 });
 
 app.Run();
