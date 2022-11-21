130d4b4506e916cd29b240d37e3bb5212a18297b
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-21 14:26:00 +0100

    Query String

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,15 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// app.MapGet("/", () => "Hello World!");
-
 app.Run(async (HttpContext context) =>
 {
-    string path = context.Request.Path;
-    string method = context.Request.Method;
-
-    await context.Response.WriteAsync($"<p>{path}</p>");
-    await context.Response.WriteAsync($"<p>{method}</p>");
+    context.Response.Headers["Content-type"] = "text/html";
+    if(context.Request.Method== "GET")
+    {
+        if(context.Request.Query.ContainsKey("id"))
+        {
+            string id = context.Request.Query["id"];
+            await context.Response.WriteAsync($"<p>{id}</p>");
+        }
+    }
 });
 
 app.Run();
